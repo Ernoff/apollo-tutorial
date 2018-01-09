1f07f6b2cc0b23ed890e94580e343187733a0429
Ernoff <ernest.offiong@gmail.com>
2018-01-09 10:45:38 +0100

    Routing done

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -9,6 +9,8 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import { BrowserRouter } from 'react-router-dom'
+
 const httpLink = new HttpLink({uri: 'https://api.graph.cool/simple/v1/cjc6kt4kn05am0128e411za60'})
 
 const client = new ApolloClient({
@@ -17,9 +19,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-  <App />
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
   , document.getElementById('root'));
   
 registerServiceWorker();
