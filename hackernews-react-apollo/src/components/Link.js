fa591ecc58b92a5deef96651bd081f367a965549
Ernoff <ernest.offiong@gmail.com>
2018-01-11 16:32:17 +0100

    Fix: Corrected time display

--- hackernews-react-apollo/src/components/Link.js
+++ hackernews-react-apollo/src/components/Link.js
@@ -17,8 +17,8 @@ class Link extends Component {
         </div>
         <div className='ml1'>
           <div> {this.props.link.description} ({this.props.link.url}) </div>
-          <div className='f6 lh-copy gray'>{this.props.link.votes.length} Votes | by {this.props.link.postedBy ? this.props.link.postedBy.id : 'Unknown'}
-          {this.props.link.createdAt ? timeDifferenceForDate(this.props.link.createdAt) : '  no date'}</div>
+          <div className='f6 lh-copy gray'>
+          {this.props.link.votes.length} Votes | by {this.props.link.postedBy ? this.props.link.postedBy.id : 'Unknown'} | {this.props.link.createdAt ? timeDifferenceForDate(this.props.link.createdAt) : '  no date'}</div>
         </div>
       </div>
     )
